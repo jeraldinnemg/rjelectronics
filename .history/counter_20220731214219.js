6960fc84d8ab487c51aaf82b1984589cee14ac9f
jeraldinnemg <jeraldinnemg@gmail.com>
2022-07-31 23:06:23 -0300

    first commit: langing page v1.0

---
+++ .history/counter_20220731214219.js
@@ -0,0 +1,47 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText='0';
+
+    const updateCounter = () => {
+
+        // Create const
+        const target = +counter.getAttribute('data-target')
+
+         
+         const c = +counter.innerText 
+
+         const increment = target / 300;
+
+         if (c < target) {
+             counter.innerText = `${Math.ceil(c+increment)}`
+             setTimeout(updateCounter,100)
+         } else {
+             counter.innerText = target
+         }
+         // verify target function console.log(typeof target, target)
+         //console.log(increment)
+    }
+
+    updateCounter()
+
+    const showCounter= elements =>{
+        elements.forEach(e => {
+            if(e.isIntersecting){
+                e.target.classList.add('animate')
+                e.target.classList.remove('hide')
+                setTimeout(updateCounter, 500)
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(showCounter, {
+        threshold:0.85 // 0-1
+    })
+
+    const elementsHTML = document.querySelectorAll('.counter');
+    elementsHTML.forEach(elementHTML =>{
+        observer.observe(elementHTML)
+    }) 
+
+})
